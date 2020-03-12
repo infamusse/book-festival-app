b5d2ad45d792ce900ebfc31b8667da8a46784247
mjalowiczor <m.jalowiczor@hotmail.com>
2020-03-12 19:01:37 +0100

    add bulid version

---
+++ client/build/precache-manifest.e0905224984a338062d3212102ece45c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0d54947017083c9afccadd52f6d6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb61eda1f50019ac22e0",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "04128cdd3d9573f0681d",
+    "url": "/static/css/main.954010d4.chunk.css"
+  },
+  {
+    "revision": "bb61eda1f50019ac22e0",
+    "url": "/static/js/2.b8ef3ed4.chunk.js"
+  },
+  {
+    "revision": "04128cdd3d9573f0681d",
+    "url": "/static/js/main.97a49b0c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
